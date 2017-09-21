017d7385170ac06052dc0c07bd8b4d9817a9dfc3
Alcardo Terlonge <alcardodevelopment@gmail.com>
2017-09-21 14:44:10 -0700

    load the index.php file

---
+++ home.php
@@ -0,0 +1,212 @@
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center teal-text text-lighten-2">Chirstiana school</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Motto</h5>
+        </div>
+        <div class="row center">
+         <!-- <a href="" class="btn-large waves-effect waves-light yellow lighten-1">Make a Donation</a> -->
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="background/background1.jpg" alt=" background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+      <div class="row">
+        <!--Images should be added here-->
+        <div class="col s12 m4">
+          <h3></h3>
+            <div class="card z-depth-5">
+                <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="background/background4.jpg">                        
+                </div>
+                <div class="card-content">
+                    <span class="card-title activator blue-text text-darken-3">Chairman's Message<i class="small material-icons right">view_headline</i></span>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title blue-text text-darken-3">Chairman's Message<i class="small material-icons right">done_all</i></span>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <a href="#">read more<i class=" material-icons">navigate_next</i></a>
+                </p>
+                </div>
+            </div>
+        </div>
+            <div class="col s12 m4">
+                <div class="card z-depth-5">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="background/background4.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator blue-text text-darken-3">Chairman's Message<i class="small material-icons right">view_headline</i></span>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title blue-text text-darken-3">Chairman's Message<i class="small material-icons right">done_all</i></span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="card z-depth-5">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="background/background4.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator blue-text text-darken-3">Chairman's Message<i class="small material-icons right">view_headline</i></span>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title blue-text text-darken-3">Chairman's Message<i class="small material-icons right">done_all</i></span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card z-depth-5">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="background/background1.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator blue-text text-darken-3">Chairman's Message<i class="small material-icons right">view_headline</i></span>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title blue-text text-darken-3">Chairman's Message<i class="small material-icons right">done_all</i></span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card z-depth-5">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="background/background1.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator blue-text text-darken-3">Chairman's Message<i class="small material-icons right">view_headline</i></span>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title blue-text text-darken-3">Chairman's Message<i class="small material-icons right">done_all</i></span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>    
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+       
+      </div>
+    </div>
+    <div class="parallax"><img src="background/background2.jpg" alt="background img 2"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+      <!-- icons must be bigger-->
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Contact Us</h4>
+          <div class="col s12 m4">
+              <div class="icon-block">
+               
+                  <i class="large material-icons">phone</i>
+                <p class="light">+876 xxx xxxx</p>
+              </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="icon-block">
+                 
+                    <i class="large material-icons">location_on</i>
+                  <p class="light">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                 
+                    <i class="large material-icons">mail</i>
+                  <p class="light">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+              <br>
+              <br>
+              <br>
+              <br>
+              <div class="input-field col s12 m6">
+                  <i class="material-icons prefix">person</i>
+                  <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                  <label for="icon_prefix2">Name</label>
+                </div>
+
+                <div class="input-field col s12 m6">
+                    <i class="material-icons prefix">email</i>
+                    <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                    <label for="icon_prefix2">Email</label>
+                  </div>
+                
+                  <div class="input-field col s12">
+                      <i class="material-icons prefix">message</i>
+                      <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                      <label for="icon_prefix2">Message</label>
+                    </div>
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                        <i class="material-icons right">send</i>
+                      </button>
+        </div>
+        
+      </div>
+
+      
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        
+      </div>
+    </div>
+    <!--placeholder for google map-->
+    <div class="parallax "><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3790.7351362656073!2d-77.49310344881937!3d18.
+        176111237549694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8edbb995d6fe8873%3A0x67a35b6a70d7549d!2sChristiana+Moravian+Primary+
+        School%2C+Christiana!5e0!3m2!1sen!2sjm!4v1505991661750" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen c></iframe></div>
+  </div>
